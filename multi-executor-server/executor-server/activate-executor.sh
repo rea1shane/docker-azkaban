8b5d3624a0c5cae94ab4ca7bca58f212e6b04baa
Shane <innon.shen@gmail.com>
2023-06-13 15:56:12 +0800

    delete http://

--- multi-executor-server/executor-server/activate-executor.sh
+++ multi-executor-server/executor-server/activate-executor.sh
@@ -4,7 +4,7 @@
 while [ $(ls | grep executorServerLog > /dev/null; echo $?) -ne 0 ] || \
     [ $(grep "Started SocketConnector" $(ls -lt . | grep executorServerLog | head -n 1 | awk '{print $9}') > /dev/null; echo $?) -ne 0 ] || \
     # https://azkaban.readthedocs.io/en/latest/getStarted.html#installing-azkaban-executor-server
-    [ $(curl --silent -G "http://localhost:$(cat ./executor.port)/executor?action=activate" > /dev/null; echo $?) -ne 0 ]
+    [ $(curl --silent -G "localhost:$(cat ./executor.port)/executor?action=activate" > /dev/null; echo $?) -ne 0 ]
 do
     sleep 1
 done
