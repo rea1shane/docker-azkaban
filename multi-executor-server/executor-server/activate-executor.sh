f97a1385ba7044f7d0586849cb668b6d04a88caa
Shane <innon.shen@gmail.com>
2023-06-13 15:39:15 +0800

    Update activate-executor.sh

--- multi-executor-server/executor-server/activate-executor.sh
+++ multi-executor-server/executor-server/activate-executor.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-# Log file not exist or server not started
-while [ $(ls | grep executorServerLog > /dev/null; echo $?) -ne 0 ] || [ $(grep "Started SocketConnector" $(ls -lt . | grep executorServerLog | head -n 1 | awk '{print $9}') > /dev/null; echo $?) -ne 0 ]
+# [ log file not exist ] or [ server not started ] or [ request failure ]
+while [ $(ls | grep executorServerLog > /dev/null; echo $?) -ne 0 ] || \
+    [ $(grep "Started SocketConnector" $(ls -lt . | grep executorServerLog | head -n 1 | awk '{print $9}') > /dev/null; echo $?) -ne 0 ] || \
+    # https://azkaban.readthedocs.io/en/latest/getStarted.html#installing-azkaban-executor-server
+    [ $(curl --silent -G "http://localhost:$(cat ./executor.port)/executor?action=activate" > /dev/null; echo $?) -ne 0 ]
 do
     sleep 1
 done
 
-# https://azkaban.readthedocs.io/en/latest/getStarted.html#installing-azkaban-executor-server
-curl -G "http://localhost:$(cat ./executor.port)/executor?action=activate" && echo
+echo "Executor has been started and activated."
