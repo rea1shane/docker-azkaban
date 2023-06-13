cce16d34837d7eaef941edd2fcaa6e205178892c
Shane <innon.shen@gmail.com>
2023-06-13 15:08:43 +0800

    Update activate-executor.sh

--- multi-executor-server/executor-server/activate-executor.sh
+++ multi-executor-server/executor-server/activate-executor.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-while [ $(grep "Started SocketConnector" $(ls -lt . | grep executorServerLog | head -n 1 | awk '{print $9}') > /dev/null; echo $?) -ne 0 ]
+# Log file not exist or server not started
+while [ $(ls | grep executorServerLog > /dev/null; echo $?) -ne 0 ] || [ $(grep "Started SocketConnector" $(ls -lt . | grep executorServerLog | head -n 1 | awk '{print $9}') > /dev/null; echo $?) -ne 0 ]
 do
     sleep 1
 done
