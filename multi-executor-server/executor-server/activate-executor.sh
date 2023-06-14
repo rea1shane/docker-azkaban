e333e6cf9acc1c9c0e1a01a6550d74b09af3c03c
Shane <innon.shen@gmail.com>
2023-06-14 12:01:19 +0800

    trap stop signal

--- multi-executor-server/executor-server/activate-executor.sh
+++ multi-executor-server/executor-server/activate-executor.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+script_dir=$(dirname $0)
+
 # [ log file not exist ] or [ server not started ] or [ request failure ]
-while [ $(ls | grep executorServerLog > /dev/null; echo $?) -ne 0 ] || \
-    [ $(grep "Started SocketConnector" $(ls -lt . | grep executorServerLog | head -n 1 | awk '{print $9}') > /dev/null; echo $?) -ne 0 ] || \
+while [ $(ls ${script_dir}/.. | grep executorServerLog > /dev/null; echo $?) -ne 0 ] || \
+    [ $(grep "Started SocketConnector" $(ls -lt ${script_dir}/.. | grep executorServerLog | head -n 1 | awk '{print $9}') > /dev/null; echo $?) -ne 0 ] || \
     # https://azkaban.readthedocs.io/en/latest/getStarted.html#installing-azkaban-executor-server
-    [ $(curl --silent -G "localhost:$(cat ./executor.port)/executor?action=activate" > /dev/null; echo $?) -ne 0 ]
+    [ $(curl --silent -G "localhost:$(cat ${script_dir}/../executor.port)/executor?action=activate" > /dev/null; echo $?) -ne 0 ]
 do
     sleep 1
 done
