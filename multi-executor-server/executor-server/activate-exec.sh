9c98665e84d67285c8b115d9c53cb41d0856526d
Shane <innon.shen@gmail.com>
2023-06-21 15:10:06 +0800

    2>&1

--- multi-executor-server/executor-server/activate-exec.sh
+++ multi-executor-server/executor-server/activate-exec.sh
@@ -3,9 +3,9 @@
 script_dir=$(dirname $0)
 
 # [ port file not exist ] or [ request failure ]
-while [ $(ls ${script_dir}/../executor.port > /dev/null; echo $?) -ne 0 ] || \
+while [ $(ls ${script_dir}/../executor.port > /dev/null 2>&1; echo $?) -ne 0 ] || \
     # https://azkaban.readthedocs.io/en/latest/getStarted.html#installing-azkaban-executor-server
-    [ $(curl --silent -G "localhost:$(cat ${script_dir}/../executor.port)/executor?action=activate" > /dev/null; echo $?) -ne 0 ]
+    [ $(curl -G "localhost:$(cat ${script_dir}/../executor.port)/executor?action=activate" > /dev/null 2>&1; echo $?) -ne 0 ]
 do
     sleep 1
 done
