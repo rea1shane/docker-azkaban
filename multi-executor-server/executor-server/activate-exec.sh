e63ba86f4028edb74d7749f97ba56543d88ee6ea
Shane <innon.shen@gmail.com>
2023-06-21 14:45:33 +0800

    Update activate-exec.sh

--- multi-executor-server/executor-server/activate-exec.sh
+++ multi-executor-server/executor-server/activate-exec.sh
@@ -2,9 +2,8 @@
 
 script_dir=$(dirname $0)
 
-# [ log file not exist ] or [ server not started ] or [ request failure ]
-while [ $(ls ${script_dir}/.. | grep executorServerLog > /dev/null; echo $?) -ne 0 ] || \
-    [ $(grep "Started SocketConnector" $(ls -lt ${script_dir}/.. | grep executorServerLog | head -n 1 | awk '{print $9}') > /dev/null; echo $?) -ne 0 ] || \
+# [ port file not exist ] or [ request failure ]
+while [ $(ls ${script_dir}/../executor.port > /dev/null; echo $?) -ne 0 ] || \
     # https://azkaban.readthedocs.io/en/latest/getStarted.html#installing-azkaban-executor-server
     [ $(curl --silent -G "localhost:$(cat ${script_dir}/../executor.port)/executor?action=activate" > /dev/null; echo $?) -ne 0 ]
 do
